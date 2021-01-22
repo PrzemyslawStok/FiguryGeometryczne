408af155924522e84a6250be26f2f47a34689e97
Przemyslaw Stoklosa <przemyslaw.stoklosa@gmail.com>
2021-01-22 17:05:40 +0100

    cd..

---
+++ src/main/kotlin/Figura2D.kt
@@ -0,0 +1,13 @@
+class Figura2D {
+    fun obliczPole():Double{
+        return 0.0
+    }
+    fun obliczObwod():Double{
+        return 0.0
+    }
+
+    fun info(){
+        println("Pole powierzchni wynosi: ${obliczPole()}")
+        println("Obwód figury wynosi: ${obliczObwod()}")
+    }
+}
--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -1,3 +1,4 @@
-fun main(args: Array<String>) {
-    println("Hello World!")
+fun main() {
+    val figura1 = Figura2D()
+    figura1.info()
 }
